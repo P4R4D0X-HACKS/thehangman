7cdf61bdb510002df13008cde132cbe7a2d094c2
P4R4D0X <prdx2001@gmail.com>
2023-09-09 05:35:49 +0530

    The main.py is changed

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 import random
 from hangman_word import word_list
-from hangman_art import logo, stages
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
-end_of_game = False
-lives = 6
-print(logo)
-display = []
-for _ in range(word_length):
-    display += "_"
+from hangman_art import stages, logo
 
+print(logo)
+life = 6
+# randomly choose a word form the list
+chosen_word = random.choice(word_list)
+# creating an empty list for each letter in chosen word adding "_"
+display = []
+word_len = len(chosen_word)
+for letter in range(word_len):
+    display += "_"
+print(display)
+
+end_of_game = False
 while not end_of_game:
+    # ask the user to guess a letter and assign their answer to a variable called guess. Make the guess lowercase
     guess = input("Guess a letter: ").lower()
     if guess in display:
         print(f"You've already guessed {guess}")
-
-    for position in range(word_length):
+    # replacing the underscore with the letter that the user choose if it is right
+    for position in range(word_len):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-
-    # Check if user is wrong.
     if guess not in chosen_word:
-
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
-
-        lives -= 1
-        if lives == 0:
+        life -= 1
+        if life == 0:
             end_of_game = True
-            print("You lose.")
-
-    print(f"{' '.join(display)}")
+            print("You Lose!")
+            print(f"The correct word was {chosen_word}")
+    print(display)
     if "_" not in display:
         end_of_game = True
-        print("You win.")
-
-    print(stages[lives])
+        print("You Win!")
+    print(stages[life])
